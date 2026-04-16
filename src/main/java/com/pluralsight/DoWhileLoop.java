cb720b6bae7e8c19ca52fc2a250ed3fced3d3760
sgrinley <shamar808@yahoo.com>
2026-04-16 11:59:12 -0700

    display: Do/While loop results

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -3,5 +3,20 @@ package com.pluralsight;
 public class DoWhileLoop {
     public static void main(String[] args) {
 
+//  Step 1: Create counter variable
+        int count = 1;
+
+//  Step 2: Create do/while loop
+        do {
+
+//  Step 3: Print message
+            System.out.println("I love Java");
+
+//  Step 4: Increase counter
+            count++;
+
+//  Step 5: Check condition after running code
+
+        } while (count <= 5);
     }
 }
