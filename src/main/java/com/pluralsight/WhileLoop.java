dd2fb8ff7b684ff55afa0828e798b575e3f39d04
sgrinley <shamar808@yahoo.com>
2026-04-16 11:43:12 -0700

    Initial commit

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -3,6 +3,17 @@ package com.pluralsight;
 public class WhileLoop {
     public static void main(String[] args) {
 
+//  Step 1: Create a counter variable
+        int count = 1;
 
+//  Step 2: Create a While Loop (will run as long as count <= 5)
+        while (count <= 5) {
+
+//  Step 3: Print message
+            System.out.println("I love Java");
+
+//  Step 4: Increase the counter (Loop will run forever if forgotten)
+            count++;
+        }
     }
 }
