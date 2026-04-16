8807d9169a113a46ce865a0f375a6d9cb02ffe84
sgrinley <shamar808@yahoo.com>
2026-04-16 12:21:58 -0700

    display: For Loop results

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,7 +1,19 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws InterruptedException {
+
+//  Step 1: Create For Loop
+        for (int i = 10; i >= 1; i--) {
+
+//  Step 2: Print current number
+            System.out.println(i);
+
+//  Step 3: Pause for a second (1000 millisecond)
+            Thread.sleep(1000);
+        }
+//  Step 4: Print final message after loop ends
+        System.out.println("Launch!");
     }
 }
